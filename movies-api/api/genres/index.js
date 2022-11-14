49b72117491a9d96a566d1c090167b43441a90d6
Adrian Bernacki <adrianbernacki2021@gmail.com>
2022-11-14 18:06:56 +0000

    Week 9 Lab exercise done

---
+++ movies-api/api/genres/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { genres } from './genresData';
+import uniqid from 'uniqid'
+
+const router = express.Router(); 
+
+router.get('/', (req, res) => {
+    res.json(genres);
+});
+
+// Get movie details
+router.get('/', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (genres.id == id) {
+        res.status(200).json(genres);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
+
+
+
+
+export default router;
