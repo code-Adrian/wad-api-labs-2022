5a4cbc73b1f5ba33babcb30f0faa47a91b8493b5
Adrian Bernacki <adrianbernacki2021@gmail.com>
2022-11-21 16:17:18 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/genres',genresRouter);
 app.use('/api/movies', moviesRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
